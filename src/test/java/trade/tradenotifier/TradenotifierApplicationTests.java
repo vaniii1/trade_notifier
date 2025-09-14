a7922e71cbaac6996d5259c1d5812f46fc911973
ivanhromovyi <ivanh@uia.no>
2025-09-14 17:19:39 +0200

    delete test method

--- src/test/java/trade/tradenotifier/TradenotifierApplicationTests.java
+++ src/test/java/trade/tradenotifier/TradenotifierApplicationTests.java
@@ -1,13 +1,8 @@
 package trade.tradenotifier;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TradenotifierApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
